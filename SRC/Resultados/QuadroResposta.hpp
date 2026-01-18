a1e047b62694ced5af4408d50b6599e722079ea1
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-18 11:06:03 -0300

    Refatora a classe QuadroResposta para melhorar a formatação de saída e o tratamento de dados.

--- SRC/Resultados/QuadroResposta.hpp
+++ SRC/Resultados/QuadroResposta.hpp
@@ -7,7 +7,9 @@ using namespace std;
 typedef vector<double> vf;
 typedef vector<vector<double>> Matriz;
 
-
+const string RED   = "\033[31m";
+const string GREEN = "\033[32m";
+const string RESET = "\033[0m";
 
 class QuadroResposta {
 private:
@@ -19,7 +21,7 @@ private:
         QuadroResposta(string titulo, vf dados, double a) : titulo(titulo), dados(dados), a(a) {}
 
         void imprime() {
-            cout << "\n\n==============================================\n";
+            cout << "\n==============================================\n";
             cout << " " << titulo << "\n";
             cout << "==============================================\n";
 
@@ -32,13 +34,15 @@ private:
             cout << "----------------------------------------------\n";
 
             for (int i = 0; i < (int)dados.size(); i++) {
-                double A = a * dados[i];
-                string situacao = (fabs(A) > 3.0) ? "\033[31mROMPE\033[0m" : "\033[32mSEGURA\033[0m";
+                double di = (fabs(dados[i]) < 1e-6) ? 0.0 : dados[i];
+                double A = a * di;
 
-                if(fabs(dados[i]) < 1e-6) dados[i] = 0;
+                string situacao = (fabs(A) > 3.0)
+                    ? RED + "ROMPE" + RESET
+                    : GREEN + "SEGURA" + RESET;
 
                 cout << setw(6)  << i + 1
-                    << setw(15) << fixed << setprecision(5) << dados[i]
+                    << setw(15) << fixed << setprecision(5) << di
                     << setw(15) << A
                     << situacao << endl;
             }
