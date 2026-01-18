9f24c1bb586fad906fd076bbc7db512a922655de
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-18 12:45:45 -0300

    Refatora a função get_dCramer para melhorar a verificação de determinante e a legibilidade do código.

--- SRC/Metodos/Cramer.hpp
+++ SRC/Metodos/Cramer.hpp
@@ -4,42 +4,43 @@
 #include "Gauss.hpp"
 #include "GaussJordan.hpp"
 
-
 using namespace std;
 typedef vector<vector<double>> Matriz;
 typedef vector<double> vf;
 
 vf get_dCramer(GaussAbs &G, const vf &v){
     int n = G.getOrdem();
-    double detA;
-    detA = G.get_detGauss();
-    if(detA == 0){
+    double detA = G.get_detGauss();
+    double EPS = 1e-6;
+
+    if(abs(detA) < EPS){
         throw runtime_error("Sistema nao possui solucao unica.");
     }
+
     vf d(n);
+
     for(int i=0; i<n; i++){
         Matriz Ai = G.getMatriz();
+
         for(int j=0; j<n; j++){
             Ai[j][i] = v[j];
         }
+
         GaussAbs *Gi;
+
         if(dynamic_cast<GaussJordan*>(&G)){
             Gi = new GaussJordan(Ai);
-        }
-        else{
+        } else{
             Gi = new Gauss(Ai);
         }
 
         double detAi;
         detAi = Gi->get_detGauss();
+
         d[i] = detAi / detA;
     }
+
     return d;
 }
-
-
-
-
-
 
 #endif
