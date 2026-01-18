0f25bf9e13fe8c12a13002d46ee09f1551b95eff
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-18 15:02:19 -0300

    Especializa Gauss e GaussJordan através de passoElimina.

--- SRC/Metodos/Gauss.hpp
+++ SRC/Metodos/Gauss.hpp
@@ -12,46 +12,18 @@ class Gauss : public GaussAbs
 public:
     Gauss(Matriz M) : GaussAbs(M) {}
 
-    double get_detGauss(){
-        double det = 1.0;
-        int trocas = 0;
-        double EPS = 1e-6;
-        Matriz tempM = getMatriz();
+    GaussAbs* clone(const Matriz& M) const override { return new Gauss(M); }
 
-        for(int i=0; i<n; i++){
-            int pivot = i;
+protected:
+    void passoElimina(Matriz& A, int i) {
+        for(int j=i+1; j<n; j++){
+            double fator = A[j][i] / A[i][i];
 
-            for(int k=i+1; k<n; k++){
-                if (abs(tempM[k][i]) > abs(tempM[pivot][i])) {
-                    pivot = k;
-                }
-            }
-
-            if (abs(tempM[pivot][i]) < EPS) return 0.0;
-
-            if (pivot != i) {
-                swap(tempM[i], tempM[pivot]);
-                trocas++;
-            }
-            
-            for(int j=i+1; j<n; j++){
-                double fator = tempM[j][i] / tempM[i][i];
-
-                for(int k=i; k<n; k++){
-                    tempM[j][k] -= fator * tempM[i][k];
-                }
+            for(int k=i; k<n; k++){
+                A[j][k] -= fator * A[i][k];
             }
         }
-
-        for(int i=0; i<n; i++){
-            det *= tempM[i][i];
-        }
-
-        if(trocas % 2 == 1) det = -det;
-
-        return det;
     }
-
 };
 
 #endif 
--- SRC/Metodos/GaussJordan.hpp
+++ SRC/Metodos/GaussJordan.hpp
@@ -11,47 +11,21 @@ class GaussJordan : public GaussAbs
 {
 public:
     GaussJordan(Matriz M) : GaussAbs(M) {}
-    
-    double get_detGauss() {
-        double det = 1.0;
-        int trocas = 0;
-        double EPS = 1e-6;
-        Matriz tempM = getMatriz();
 
-        for(int i=0; i<n; i++){
-            int pivotL = i;
+    GaussAbs* clone(const Matriz& M) const override { return new GaussJordan(M); }
 
-            for(int k=i+1; k<n; k++){
-                if (abs(tempM[k][i]) > abs(tempM[pivotL][i])) pivotL = k;
-            }
+protected:
+    void passoElimina(Matriz& A, int i) {
+        for(int j=0; j<n; j++){
+            if(j == i) continue;
 
-            if (abs(tempM[pivotL][i]) < EPS) return 0.0;
+            double fator = A[j][i] / A[i][i]; 
 
-            if (pivotL != i){
-                    swap(tempM[i], tempM[pivotL]);
-                    trocas++;
-            }
-
-            for(int j=0; j<n; j++){
-                if(j == i) continue;
-
-                double fator = tempM[j][i] / tempM[i][i]; 
-
-                for(int k=i; k<n; k++){
-                    tempM[j][k] -= fator * tempM[i][k];
-                }
+            for(int k=i; k<n; k++){
+                A[j][k] -= fator * A[i][k];
             }
         }
-
-        for(int i=0; i<n; i++){
-            det *= tempM[i][i];
-        }
-
-        if(trocas % 2 == 1) det = -det;
-        
-        return det;
     }
-
 };
 
 #endif
